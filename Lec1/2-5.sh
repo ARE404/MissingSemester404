6ec7511725229aeea70fdeffae94bf6d749a0554
ARE404 <66726968+ARE404@users.noreply.github.com>
2022-03-29 11:00:07 +0800

    Update and rename 5.sh to 2-5.sh

---
+++ Lec1/2-5.sh
@@ -0,0 +1,12 @@
+# Create a new directory called missing under /tmp.
+# Use touch to create a new file called semester in missing.
+# Write the following into that file, one line at a time:
+# #!/bin/sh
+# curl --head --silent https://missing.csail.mit.edu
+
+mkdir /tmp/semester
+cd /tmp/semester
+touch missing
+echo '#!/bin/sh' > semester #can only use signle quote for !
+echo "curl --head --silent https://missing.csail.mit.edu" >> semester
+vim semester
--- Lec1/5.sh
+++
@@ -1,6 +0,0 @@
-mkdir /tmp/semester
-cd /tmp/semester
-touch missing
-echo '#!/bin/sh' > semester #can only use signle quote for !
-echo "curl --head --silent https://missing.csail.mit.edu" >> semester
-vim semester
